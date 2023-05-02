2114db950b4a63fc09abe6f832dab190a4c2cd87
LudvigDillen <luddi824@student.liu.se>
2023-05-02 15:26:05 +0200

    Add functionality to read entire sub-dataset of nuscenes

--- main.py
+++ main.py
@@ -14,13 +14,35 @@ from visualization.point_clouds import vis_pc, vis_2pcs_aligned_vs_misaligned
 
 def main():
     print("cuda available:", torch.cuda.is_available())
+    # TODO: 2023-04-27 (Gather data into lists e.g.)
+    # 1. Gather data into lists (DONE!)
+    # 2. Split data up into training and test set
+    # 3. Perform random perturbations or register data with ICP e.g.
+    # 3.1 With ICP we can compare the estimate transformation by with the ground truth to classify
+    #     the data as either aligned or misaligned. This data is then quite realistic and can be
+    #     used to train our classifier (select suitable parameters).
+    # 3.2 Align point clouds with ground truth and then apply a small offset and then register
+    #     with ICP. This can lead to misalignments which are harder to detect as the relative
+    #     error transformation is smaller. Thus, we might challenge the model a bit more which is
+    #     good. Furthermore, ICP is a very interesting registration model to use for three reasons,
+    #       1. Implementations of ICP are easily accesible thorugh e.g. Open3d
+    #       2. It easily get stuck in local minima which is the realistic/common point when registration
+    #          methods stop to iterate.
+    #       3. It is common and well known, and useful as a benchmark registration method in that sense.
+
+    # TODO: How to choose model parameters
+    # 1. Use some optimization in PyTorch to backpropagate the parameters of the logistic regression
+    #    model (beta0, beta1, beta2).
+    # 2. Optimize of the other parameters alpha (even though it is given by the dataset), epsilon,
+    #    and E_reject by optimizing over a grid of values (grid search).
 
     # Initialize NuScenes object
     nusc = ns.nuscenes.NuScenes(version='v1.0-mini', dataroot='data/nuscenes/mini/', verbose=True)
     PCHandler = NuscenesHandling(nusc, downsample_factor=1)
-    PC0 = PC(PCHandler.pc0_CS0, PCHandler.point_distances0)
-    PC1 = PC(PCHandler.pc1_CS1, PCHandler.point_distances1)
 
+    PC_scenes = PCHandler.get_entire_sub_dataset()
+
+    exit()
     pc_union_dists = torch.cat((PC0.distances_to_origin, PC1.distances_to_origin))
 
     # Calculate differential entropy
@@ -29,13 +51,14 @@ def main():
 
     # Some visualization: We can see that the point clouds are better aligned after the transformation
     # vis_2pcs_aligned_vs_misaligned(PC0.pc, PC1.pc, pc1_CS0)
-    #
+    ##
+    # exit()
 
     pc_union_after_alignment = torch.cat((PCHandler.pc0_CS0, pc1_CS0), dim=0)
     PCUnion_after_alignment = PC(pc_union_after_alignment, pc_union_dists)
     t1 = time.time()
     result = differential_entropy_metric(PC0, PC1, PCUnion_after_alignment)
-    print(f"Differential entropy after alignment: {result}")
+    print(f"Differential entropy after alignment: {np.around(result.cpu().numpy(), decimals=3)}")
     print(f"Execution time (sec): {round(time.time() - t1, 3)}")
     print("Finito!")
     # TODO: Compare the differential entropy metric before and after the alignment.
--- utils/nuscenes_handling.py
+++ utils/nuscenes_handling.py
@@ -1,35 +1,21 @@
 import numpy as np
 import torch
-from utils.geometrics import transformation_matrix
 
+from utils.geometrics import transformation_matrix
+from utils.pointclouds import PC, PCPair
 from nuscenes.utils.data_classes import LidarPointCloud
 
 
 class NuscenesHandling:
     def __init__(self, nusc, downsample_factor=1, lidar_token=None):
         self.nusc = nusc
-
-        # Set info PC0
-        if lidar_token is None:
-            self.first_token = self.get_first_token()
-            self.lidar_token0 = self.get_lidar_token(self.first_token)
-        else:
-            self.lidar_token0 = lidar_token
-        self.lidar_pose0 = self.get_sensor_pose_in_WCS(self.lidar_token0)
-        self.pc0_CS0 = self.get_point_cloud(self.lidar_token0)
-
-        # Set info PC1
-        self.lidar_token1 = self.get_next_lidar_token(self.lidar_token0)
-        self.lidar_pose1 = self.get_sensor_pose_in_WCS(self.lidar_token1)
-        self.pc1_CS1 = self.get_point_cloud(self.lidar_token1)
-
+        self.scene_counter = 0
+        self.number_of_scenes_in_dataset = len(self.nusc.scene)
+        self.dataset_read = False
+        self.scene_read = False
         # Randomly downsample point clouds
         self.downsample_factor = downsample_factor
-        if self.downsample_factor > 1:
-            self.downsample_both_point_clouds()
-
-        self.point_distances0 = self.get_point_distances_to_origin(self.pc0_CS0)
-        self.point_distances1 = self.get_point_distances_to_origin(self.pc1_CS1)
+        self.setup_new_scene_data(lidar_token)
 
     def downsample_both_point_clouds(self):
         N_samples_before = self.pc0_CS0.shape[0]  # Assuming both point clouds have equally many points
@@ -43,13 +29,49 @@ class NuscenesHandling:
     def downsample_second_point_cloud(self):
         self.pc1_CS1 = self.pc1_CS1[self.samples_to_keep]  # Downsample point cloud
 
-    def get_first_token(self):
-        # Get the token to the first sample in the first scene
-        return self.nusc.scene[0]['first_sample_token']
+    def setup_new_scene_data(self, lidar_token=None):
+        # Set info PC0
+        if lidar_token is None:
+            self.first_sample_token = self.get_first_sample_token()
+            self.lidar_token0 = self.get_lidar_token(self.first_sample_token)
+        else:
+            self.lidar_token0 = lidar_token
+        self.lidar_pose0 = self.get_sensor_pose_in_WCS(self.lidar_token0)
+        self.pc0_CS0 = self.get_point_cloud(self.lidar_token0)
 
-    def get_lidar_token(self, token):
-        # Get the token for the LIDAR_TOP sensor in the first sample
-        return self.nusc.get('sample', token)['data']['LIDAR_TOP']
+        # Set info PC1
+        self.lidar_token1 = self.get_next_lidar_token(self.lidar_token0)
+        self.lidar_pose1 = self.get_sensor_pose_in_WCS(self.lidar_token1)
+        self.pc1_CS1 = self.get_point_cloud(self.lidar_token1)
+
+        # Randomly downsample point clouds
+        self.downsample_factor = self.downsample_factor
+        if self.downsample_factor > 1:
+            self.downsample_both_point_clouds()
+
+        self.point_distances0 = self.get_point_distances_to_origin(self.pc0_CS0)
+        self.point_distances1 = self.get_point_distances_to_origin(self.pc1_CS1)
+
+    def get_first_sample_token(self):
+        # Get the sample_token to the first sample in the scene
+        return self.nusc.scene[self.scene_counter]['first_sample_token']
+
+    def get_sample_token(self, lidar_token):
+        # From lidar_token get sample token
+        return self.nusc.get('sample_data', lidar_token)['sample_token']
+
+    def get_scene_token(self, lidar_token):
+        """
+        From lidar_token get scene_token
+        """
+        # From lidar_token get sample_token
+        sample_token = self.nusc.get('sample_data', lidar_token)['sample_token']
+        # From sample_token get scene_token
+        return self.nusc.get('sample', sample_token)['scene_token']
+
+    def get_lidar_token(self, sample_token):
+        # Get the lidar_token for the LIDAR_TOP sensor from sample_token
+        return self.nusc.get('sample', sample_token)['data']['LIDAR_TOP']
 
     def get_lidar_dict(self, lidar_token):
         return self.nusc.get('sample_data', lidar_token)
@@ -94,11 +116,11 @@ class NuscenesHandling:
         return lidar_dict['next']
 
     def set_next_point_cloud_pair(self):
-        # TODO: Make sure that we can also go to the next scene ...
-        # Currently we only iterate through the current scene.
-        # I think an error will appear as we get to the end of the
-        # scene.
-
+        """
+        Iteratate to next point cloud in the dataset. If the scene is finished, we go on to
+        the next scene. If all as scenes are read, then we have read the entire dataset. Then,
+        we have collected all the data we need/want.
+        """
         # Move to next point cloud pair in the scene
         self.lidar_token0 = self.lidar_token1
         self.lidar_pose0 = self.lidar_pose1
@@ -106,6 +128,16 @@ class NuscenesHandling:
 
         # Set info PC1
         self.lidar_token1 = self.get_next_lidar_token(self.lidar_token0)
+        end_of_scene = (self.lidar_token1 == '')
+        if end_of_scene:
+            self.scene_read = True
+            self.scene_counter += 1
+            end_of_dataset = (self.scene_counter >= self.number_of_scenes_in_dataset)
+            if end_of_dataset:
+                self.dataset_read = True
+            else:
+                self.setup_new_scene_data(lidar_token=None)
+            return None
         self.lidar_pose1 = self.get_sensor_pose_in_WCS(self.lidar_token1)
         self.pc1_CS1 = self.get_point_cloud(self.lidar_token1)
 
@@ -114,3 +146,54 @@ class NuscenesHandling:
             self.downsample_second_point_cloud()
 
         self.point_distances1 = self.get_point_distances_to_origin(self.pc1_CS1)
+
+    def get_number_of_samples_in_scenes(self, PC_scenes):
+        n_samples = 0
+        for PC_scene in PC_scenes:
+            n_samples += len(PC_scene)
+        return n_samples
+
+    def get_entire_sub_dataset(self):
+        """
+        Here we return the entire sub-dataset. E.g. the mini-dataset or part1-dataset of
+        the Nuscenes dataset.
+
+        Data format:
+            A list of scenes. Each scenes correspond to roughly 20s recoreded lidar data
+            at 20 Hz. This means that each scene contains around 400 point clouds. We have
+            divided in so that the list consists of the class PCPairs which contains the
+            one point cloud pair and the corresponding union point cloud.
+        """
+        PC_scenes = []
+        PC_scene = []
+        PC0 = PC(self.pc0_CS0, self.point_distances0)
+        count = 0
+        while True:
+            if count % 2000 == 0 and count != 0:
+                print(f"We have collected {count} number of samples")
+
+            # Load in second point cloud
+            PC1 = PC(self.pc1_CS1, self.point_distances1)
+            # Set point cloud pair and their union
+            currentPCPair = PCPair(PC0, PC1, self)
+            # Append pair to list
+            PC_scene.append(currentPCPair)
+            # Iterate to next pair in scene
+            self.set_next_point_cloud_pair()
+
+            if self.scene_read == True:
+                # Append scene to list of scenes
+                PC_scenes.append(PC_scene)
+                PC_scene = []
+                self.scene_read = False
+                print(f"We have collected {self.scene_counter} scenes")
+            if self.dataset_read == True:
+                print("We have collected all data from the dataset")
+                print(f"Total number of samples: {self.get_number_of_samples_in_scenes(PC_scenes)}")
+                print(f"Total number of scenes:  {len(PC_scenes)}")
+                break
+
+            # Old PC1 is the new PC0
+            PC0 = PC1
+            count += 1
+        return PC_scenes
